986a2bd6c7752a12578bb580a08eec21a829721f
Paul Preda <ppd1512@gmail.com>
2023-05-09 15:52:45 +0100

    added the tests for the get route

--- index.test.js
+++ index.test.js
@@ -9,14 +9,27 @@ const { Musician } = require('./models/index')
 const app = require('./src/app');
 const seedMusician = require("./seedData");
 
-
 describe('./musicians endpoint', () => {
     // Write your tests here
-    
-    
 
+    test("Testing bakedGoods endpoint", async () => {
+        // Sends request to `/bakedGoods` endpoint
+        const response = await request(app).get("/musicians");
+    })
+    test("Testing bakedGoods endpoint returns the right status code", async () => {
+        // Sends request to `/bakedGoods` endpoint
+        const response = await request(app).get("/musicians");
+        expect(response.statusCode).toBe(200);
+    })
+    test("Testing bakedGoods endpoint", async () => {
+        // Sends request to `/bakedGoods` endpoint
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+        expect(response.body[0]).toHaveProperty("id")
+        expect(response.body[0]).toHaveProperty("name")
+        expect(response.body[0]).toHaveProperty("instrument")
+        expect(response.body[0]).toHaveProperty("createdAt")
+        expect(response.body[0]).toHaveProperty("updatedAt")
+    })
 
-
-
-    
 })
